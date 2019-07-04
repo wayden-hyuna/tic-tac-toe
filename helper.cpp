3ae8f31462619388ce1ca69c0284a6039152c75d
wayden-hyuna <wayden.jones@hyuna.bb>
2019-07-04 17:10:21 -0400

    prgress: almost done

--- helper.cpp
+++ helper.cpp
@@ -1,15 +1,79 @@
 #include <iostream>
 using namespace std;
+#include <map>
+#include <iterator>
+#include <vector>
+#include <string>
 
 void buildBoard(char board[3][3]);
+void playGame(char board[3][3], char Player);
+bool isWinningState(char board[3][3]);
+
+map<int, vector<int>> playerChoiceMap;
 
 void buildBoard(char board[3][3])
 {
 
-    cout << board[0][0] << "|" << board[0][1] << "|" << board[0][2] << "\n";
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            if ((x == 0 || x == 1) && (board[x][y] == ' ') && y == 2)
+                cout << "_";
+            else if ((x == 0 || x == 1) && (board[x][y] == ' ') && y != 2)
+                cout << "_|";
+            else
+            {
+                if (y != 2)
+                    cout << board[x][y] << "|";
+                else
+                    cout << board[x][y];
+            }
+        }
+        cout << endl;
+    }
 
-    cout << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << "\n";
-
-    cout << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << "\n";
     return;
+}
+
+void playGame(char board[3][3], char Player)
+{
+
+    cout << "\n\nCurrent Turn: " << Player << endl
+         << endl;
+    int playerChoice = 0;
+
+    buildBoard(board);
+    
+    playerChoiceMap[1] = {2, 0};
+    playerChoiceMap[2] = {2, 1};
+    playerChoiceMap[3] = {2, 2};
+    playerChoiceMap[4] = {1, 0};
+    playerChoiceMap[5] = {1, 1};
+    playerChoiceMap[6] = {1, 2};
+    playerChoiceMap[7] = {0, 0};
+    playerChoiceMap[8] = {0, 1};
+    playerChoiceMap[9] = {0, 2};
+
+    cout << "\nEnter the number that corresponds to where you wish to play: ";
+    cin >> playerChoice;
+    int x = 1;
+    for (int x = 1; x < 10; x++)
+    {
+        if (playerChoice == x)
+        {
+            board[playerChoiceMap[x][0]][playerChoiceMap[x][1]] = Player;
+            return;
+        }
+    }
+}
+
+bool isWinningState(char board[3][3])
+{
+    if (((board[0][0] == 'X') || (board[0][0] == 'O')) && (board[0][0] == board[1][0]) && (board[1][0] == board[2][0]))
+    {
+        return true;
+    }
+
+    return false;
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include "helper.cpp"
+
 using namespace std;
 
 int main()
 {
-    char Arr[3][3] = {{'_', '_', '_'}, {'_', '_', '_'}, {' ', ' ', ' '}};
+    char Player = ' ';
+    int roundCounter = 1;
+    char Arr[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
     bool gameStatus = true;
-    while (gameStatus)
+    do
     {
-
-        buildBoard(Arr);
-
-        if (true)
+        if (Player == 'X')
         {
-            gameStatus = false;
+            Player = 'O';
         }
-    }
+        else if (Player == 'O' or Player == ' ')
+        {
+            Player = 'X';
+        }
 
+        cout << "The number correspond to the position";
+        cout << endl;
+        cout << 7 << "|" << 8 << "|" << 9 << "\n";
+
+        cout << 4 << "|" << 5 << "|" << 6 << "\n";
+
+        cout << 1 << "|" << 2 << "|" << 3 << "\n";
+
+        playGame(Arr, Player);
+
+        roundCounter++;
+    } while (!isWinningState(Arr));
+
+    buildBoard(Arr);
+    cout << "\nPlayer " << Player << " Wins!";
     return 0;
 }
